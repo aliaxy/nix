0da17c255c72189bfc3bcb99ba3adcf70bfac29c
aliaxy <aruvelut00@163.com>
2026-05-06 11:29:53 +0800

    feat(templates/c): add C/C++ dev shell

---
+++ templates/c/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "C/C++ development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            clang
+            cmake
+            ninja
+            clang-tools
+            bear
+            pkg-config
+          ];
+
+          shellHook = ''
+            export CC=clang
+            export CXX=clang++
+          '';
+        };
+      });
+}
