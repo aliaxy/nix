fd0129f343ec67d4229ac758b9ac40d20252d9bd
aliaxy <aruvelut00@163.com>
2026-05-06 14:23:51 +0800

    feat(templates/python): reserve withPackages for project deps

--- templates/python/flake.nix
+++ templates/python/flake.nix
@@ -6,17 +6,29 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-            python3
+            (python3.withPackages (
+              ps: with ps; [
+              ]
+            ))
             uv
             ruff
             pyright
           ];
         };
-      });
+      }
+    );
 }
