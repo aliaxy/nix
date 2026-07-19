4b07ec1e065a02637de55298f234a79f2fb6c40f
aliaxy <aruvelut00@163.com>
2026-07-19 14:49:13 +0800

    feat(nix): add devenv binary cache substituter

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -3,6 +3,16 @@ _: {
   # Enable flakes and the unified CLI (nix-command)
   nix.settings.experimental-features = "nix-command flakes";
 
+  # Use devenv's public binary cache for its prebuilt dependencies.
+  nix.settings = {
+    extra-substituters = [
+      "https://devenv.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+    ];
+  };
+
   # Allow unfree/proprietary packages project-wide
   nixpkgs.config.allowUnfree = true;
 }
